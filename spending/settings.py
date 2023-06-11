5677438c512d8d6034b0fdfbdd879433799f9642
avalon <asd95027@gmail.com>
2023-06-11 13:00:42 +0800

    更換成雲段資料庫&發布用檔案

--- spending/settings.py
+++ spending/settings.py
@@ -82,6 +82,17 @@ DATABASES = {
     }
 }
 
+DATABASES = {
+    'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'spending',
+    'USER': 'avalon',
+    'PASSWORD': '12345678',
+    'HOST': 'database-3.c0y3lgknejqp.ap-northeast-1.rds.amazonaws.com',
+    'PORT': '3306',
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
